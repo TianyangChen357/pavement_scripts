23772259b97611237ce7cbdc295d38ed154e5769
TianyangChen357 <tchen19@uncc.edu>
2024-01-18 11:05:20 -0500

    initial

--- frame_extract.py
+++ frame_extract.py
@@ -7,48 +7,35 @@ import argparse
 parser = argparse.ArgumentParser(
                     prog='pavement data processing',
                     description='extracting frames from video')
-parser.add_argument('--video_dir',type=str,default='name a video directory')
-parser.add_argument('--out_dir',type=str,default='name an out directory')
-parser.add_argument('--start',type=int,default=0)
-parser.add_argument('--length',type=int,default=1000)
+parser.add_argument('--video_dir',type=str,default='/home/cagis/pavement/video/4K120fpsLinear.MP4')
+parser.add_argument('--out_folder_dir',type=str,default='/home/cagis/pavement/frame')
+parser.add_argument('--start_index',type=int,default=0)
+parser.add_argument('--nframes',type=int,default=1000)
 args=parser.parse_args()
 
-
-basedir=r'/home/cagis/pavement'
-video_folder=os.path.join(basedir,'video')
-frame_folder=os.path.join(basedir,'frame')
-start=time.time()
-# one video for testing
-video_dir=os.path.join(video_folder,'4K120fpsLinear.MP4')
+video_dir=args.video_dir
+out_folder_dir=args.out_folder_dir
+start_index=args.start_index
+nframes=args.nframes
 # load video using cv2
 cam = cv2.VideoCapture(video_dir)
 # frame
-currentframe = 0
-ret, frame = cam.read()
-print(ret)
-print(frame.shape)
+cam.set(cv2.CAP_PROP_POS_FRAMES,start_index)
 
+print(f'video directory: {video_dir}')
 length=cam.get(cv2.CAP_PROP_FRAME_COUNT)
 print(f' # frame in total: {length}')
-
 fps=length=cam.get(cv2.CAP_PROP_FPS)
 print(f'frame per second: {fps}')
-
 shutter=length=cam.get(cv2.CAP_PROP_XI_SHUTTER_TYPE)
 print(shutter)
 
-
-# for i in range(100):
-#     name=os.path.join(frame_folder,f'frame{currentframe}.jpg')
-#     ret,frame=cam.read()
-#     cv2.imwrite(name, frame)
-#     currentframe+=1
-
-for i in range(10):
-    name=os.path.join(frame_folder,f'frame{currentframe}.npy')
+currentframe=start_index
+for i in range(nframes):
+    out_name=os.path.join(out_folder_dir,f'frame{currentframe}.jpg')
     ret,frame=cam.read()
-    np.save(name,frame)
+    if ret==0:
+        break
+    cv2.imwrite(out_name, frame)
     currentframe+=1
-end=time.time()
 
-print(f'computing time: {end-start}')
