d18ed2d9f6cf70ead09c79c7fd0a4f882e9d176e
TianyangChen357 <tchen19@uncc.edu>
2024-01-22 10:39:32 -0500

    1-22-24

--- frame_extract.py
+++ frame_extract.py
@@ -9,7 +9,10 @@ parser = argparse.ArgumentParser(
                     description='extracting frames from video')
 parser.add_argument('--video_dir',type=str,default='/home/cagis/pavement/video/4K120fpsLinear.MP4')
 parser.add_argument('--out_folder_dir',type=str,default='/home/cagis/pavement/frame')
-parser.add_argument('--start_index',type=int,default=0)
+parser.add_argument('--start_index',type=int,default=20000)
+parser.add_argument('--ns',type=int,default=3)
+parser.add_argument('--nfps',type=int,default=5)
+parser.add_argument('--sampling',action='store_true')
 parser.add_argument('--nframes',type=int,default=10)
 args=parser.parse_args()
 
@@ -17,6 +20,9 @@ video_dir=args.video_dir
 out_folder_dir=args.out_folder_dir
 start_index=args.start_index
 nframes=args.nframes
+sampling=args.sampling
+ns=args.ns
+nfps=args.nfps
 # load video using cv2
 cam = cv2.VideoCapture(video_dir)
 # frame
@@ -29,12 +35,27 @@ fps=length=cam.get(cv2.CAP_PROP_FPS)
 print(f'frame per second: {fps}')
 
 
-currentframe=start_index
-for i in range(nframes):
-    out_name=os.path.join(out_folder_dir,f'frame{currentframe}.jpg')
-    ret,frame=cam.read()
-    if ret==0:
-        break
-    cv2.imwrite(out_name, frame)
-    currentframe+=1
+currentframe = start_index
+if sampling:
+    nframes=ns*nfps
+    for i in range(nframes):
+        out_name = os.path.join(out_folder_dir, f'frame{currentframe}.jpg')
+        ret, frame = cam.read()
+        if ret==0:
+            break
+        cv2.imwrite(out_name, frame)
+        interval=int(fps/nfps)
+        currentframe+=interval
+        cam.set(cv2.CAP_PROP_POS_FRAMES, currentframe)
+
+
+
+else:
+    for i in range(nframes):
+        out_name=os.path.join(out_folder_dir,f'frame{currentframe}.jpg')
+        ret,frame=cam.read()
+        if ret==0:
+            break
+        cv2.imwrite(out_name, frame)
+        currentframe+=1
 
